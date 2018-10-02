8cdf6eef03f16ff7daa788428ed0918dd2d0c4f6
roland <>
2018-10-02 07:45:34 +0000

    Initial version.

---
+++ MQTTPushClient/PushClient/ServerListTableViewCell.h
@@ -0,0 +1,14 @@
+/*
+ * $Id$
+ * This is an unpublished work copyright (c) 2018 HELIOS Software GmbH
+ * 30827 Garbsen, Germany
+ */
+
+#import <UIKit/UIKit.h>
+
+@interface ServerListTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *statusImageView;
+@property (weak, nonatomic) IBOutlet UILabel *serverNameLabel;
+
+@end
