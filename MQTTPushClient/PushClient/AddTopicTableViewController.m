2c7722f28bcd6bd538fbd73090b76b08c4d03703
roland <>
2018-10-29 10:55:05 +0000

    There is a user interface to enter the topic name.

--- MQTTPushClient/PushClient/AddTopicTableViewController.m
+++ MQTTPushClient/PushClient/AddTopicTableViewController.m
@@ -11,14 +11,38 @@
 
 @interface AddTopicTableViewController ()
 
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *subscribeBarButtonItem;
+@property (weak, nonatomic) IBOutlet UITextField *topicTextField;
+
 @end
 
 @implementation AddTopicTableViewController
 
+- (IBAction)validateFields:(id)sender {
+	self.subscribeBarButtonItem.enabled = self.topicTextField.text.length > 0;
+}
+
+- (IBAction)subscribeAction:(UIBarButtonItem *)sender {
+	NSString *topic = self.topicTextField.text;
+	if (topic.length) {
+		Connection *connection = [[Connection alloc] init];
+		[connection addTopicForAccount:self.account name:topic type:NotificationBanner];
+		[connection getTopicsForAccount:self.account];
+		[self.navigationController popViewControllerAnimated:YES];
+	}
+}
+
 - (void)viewWillAppear:(BOOL)animated {
 	[super viewWillAppear:animated];
-	Connection *connection = [[Connection alloc] init];
-	[connection addTopicForAccount:self.account name:@"bla/test" type:NotificationBanner];
+	self.subscribeBarButtonItem.enabled = NO;
+}
+
+#pragma mark - UITextFieldDelegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+	[textField resignFirstResponder];
+	self.subscribeBarButtonItem.enabled = self.topicTextField.text.length > 0;
+	return YES;
 }
 
 @end
