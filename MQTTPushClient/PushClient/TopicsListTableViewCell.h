74cf9f127f677ab4ab0ec6ae1a607e753a87202f
roland <>
2018-11-01 16:19:20 +0000

    Initial version.

---
+++ MQTTPushClient/PushClient/TopicsListTableViewCell.h
@@ -0,0 +1,14 @@
+/*
+ * $Id$
+ * This is an unpublished work copyright (c) 2018 HELIOS Software GmbH
+ * 30827 Garbsen, Germany
+ */
+
+#import <UIKit/UIKit.h>
+
+@interface TopicsListTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *topicLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *topicTypeImageView;
+
+@end
---
+++ MQTTPushClient/PushClient/TopicsListTableViewCell.m
@@ -0,0 +1,11 @@
+/*
+ * $Id$
+ * This is an unpublished work copyright (c) 2018 HELIOS Software GmbH
+ * 30827 Garbsen, Germany
+ */
+
+#import "TopicsListTableViewCell.h"
+
+@implementation TopicsListTableViewCell
+
+@end
