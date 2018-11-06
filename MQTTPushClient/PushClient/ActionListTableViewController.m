a64c17ffcca281c453c37cf5c0cf3267eba8b4ae
roland <>
2018-11-06 15:22:00 +0000

    There is editing support now for the list of actions.

--- MQTTPushClient/PushClient/ActionListTableViewController.m
+++ MQTTPushClient/PushClient/ActionListTableViewController.m
@@ -20,6 +20,22 @@
 	[self.tableView reloadData];
 }
 
+- (void)setEditing:(BOOL)editing animated:(BOOL)animated {
+	[super setEditing:editing animated:animated];
+	if (editing) {
+		[self.tableView insertRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:0 inSection:0]]
+							  withRowAnimation:UITableViewRowAnimationAutomatic];
+	} else {
+		[self.tableView deleteRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:0 inSection:0]]
+							  withRowAnimation:UITableViewRowAnimationAutomatic];
+	}
+}
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
 - (void)viewWillAppear:(BOOL)animated {
 	[super viewWillAppear:animated];
 	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateList:) name:@"ServerUpdateNotification" object:nil];
@@ -35,14 +51,29 @@
 #pragma mark - Table view data source
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+	if (self.editing)
+		return 1 + self.account.actionList.count; // because of entry "add new item" in the UI
 	return self.account.actionList.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"IDActionCell" forIndexPath:indexPath];
-	Action *action = self.account.actionList[indexPath.row];
-	cell.textLabel.text = action.name;
+	UITableViewCell *cell;
+	if (self.editing && indexPath.row == 0) {
+		cell = [tableView dequeueReusableCellWithIdentifier:@"IDAddActionCell" forIndexPath:indexPath];
+	} else {
+		cell = [tableView dequeueReusableCellWithIdentifier:@"IDActionCell" forIndexPath:indexPath];
+		Action *action = self.account.actionList[indexPath.row];
+		cell.textLabel.text = action.name;
+	}
 	return cell;
+}
+
+#pragma mark - Table view delegate
+
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
+	if (indexPath.row == 0)
+		return UITableViewCellEditingStyleInsert;
+	return UITableViewCellEditingStyleDelete;
 }
 
 #pragma mark - navigation
