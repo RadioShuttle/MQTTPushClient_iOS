57be322184171c1202e54dfb40959651307137c2
martin <>
2018-11-14 16:22:15 +0000

    Build a single notification message from all messages from the server. It can be - "topic: message" for a single message, - "topic: N new messages" for multiple messages with the same topic, or - "N new messages" in the general case.

--- MQTTPushClientServiceExtension/NotificationService.m
+++ MQTTPushClientServiceExtension/NotificationService.m
@@ -10,17 +10,12 @@
 #import "NSDictionary+HelSafeAccessors.h"
 
 @interface NotificationService ()
-
-@property (nonatomic, strong) void (^contentHandler)(UNNotificationContent *contentToDeliver);
-@property (nonatomic, strong) UNMutableNotificationContent *bestAttemptContent;
-
 @end
 
 @implementation NotificationService
 
 - (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler {
-    self.contentHandler = contentHandler;
-    self.bestAttemptContent = [request.content mutableCopy];
+    UNMutableNotificationContent *newContent = [request.content mutableCopy];
 	
 	NSDictionary *userInfo = request.content.userInfo;
 	NSString *pushServerID = [userInfo helStringForKey:@"pushserverid"];
@@ -30,19 +25,38 @@
 		NSArray<Message *>*messageList = [MessageDataHandler
 										  messageListFromRemoteMessage:userInfo
 										  forAccount:account];
+		
 		[account addMessageList:messageList];
+		newContent.badge = @(messageList.count);
+		if (messageList.count == 0) {
+			newContent.body = @"";
+		} else if (messageList.count == 1) {
+			Message *msg = messageList[0];
+			newContent.body = [NSString stringWithFormat:@"%@: %@", msg.topic, msg.content];
+		} else {
+			// Do all messages have the same topic?
+			Message *msg = messageList[0];
+			BOOL sameTopics = YES;
+			for (NSInteger i = 1; i < messageList.count ; i++) {
+				if (![messageList[i].topic isEqualToString:msg.topic]) {
+					sameTopics = NO;
+					break;
+				}
+			}
+			if (sameTopics) {
+				newContent.body = [NSString stringWithFormat:@"%@: %d new messages", msg.topic, (int)messageList.count];
+			} else {
+				newContent.body = [NSString stringWithFormat:@"%d new messages", (int)messageList.count];
+			}
+		}
 	}
 
-    // Modify the notification content here...
-    self.bestAttemptContent.title = [NSString stringWithFormat:@"%@ [modified]", self.bestAttemptContent.title];
-    
-    self.contentHandler(self.bestAttemptContent);
+    contentHandler(newContent);
 }
 
 - (void)serviceExtensionTimeWillExpire {
     // Called just before the extension will be terminated by the system.
     // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-    self.contentHandler(self.bestAttemptContent);
 }
 
 @end
