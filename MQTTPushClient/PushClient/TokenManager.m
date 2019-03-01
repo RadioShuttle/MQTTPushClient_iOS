f5ac3d2decfe87b77f1c548257f7f70bebafd583
martin <>
2019-03-01 13:43:51 +0000

    Simplified: Pending list is only maintained in user defaults, not in an instance variable. Added explaining comments.

--- MQTTPushClient/PushClient/TokenManager.m
+++ MQTTPushClient/PushClient/TokenManager.m
@@ -8,46 +8,25 @@
 #import "FIRMessaging.h"
 #import "Trace.h"
 
+/*
+ * Key for a string list user default, containing the FCM sender IDs for
+ * tokens which haven't yet been deleted due to network problems.
+ */
 static NSString *kPrefkeyPendingDeleteIDs = @"PendingDeleteIDs";
 
 @interface TokenManager ()
-
-@property NSMutableArray<NSString *> *pendingDeleteIds;
 @property dispatch_source_t timer;
 @end
 
 @implementation TokenManager
-
-// Private
-- (instancetype)init {
-	if ((self = [super init]) != nil) {
-		_pendingDeleteIds = [NSMutableArray array];
-	}
-	return self;
-}
 
 + (instancetype)sharedTokenManager {
 	static TokenManager *_sharedTokenManager;
 	static dispatch_once_t onceToken;
 	dispatch_once(&onceToken, ^{
 		_sharedTokenManager = [[TokenManager alloc] init];
-		[_sharedTokenManager load];
 	});
 	return _sharedTokenManager;
-}
-
-- (void)load {
-	NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-	NSArray *pending = [defaults stringArrayForKey:kPrefkeyPendingDeleteIDs];
-	if (pending != nil) {
-		[self.pendingDeleteIds addObjectsFromArray:pending];
-	}
-}
-
-- (void)save {
-	NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-	[defaults setValue:self.pendingDeleteIds forKey:kPrefkeyPendingDeleteIDs];
-
 }
 
 - (void)deleteTokenFor:(NSString *)senderID {
@@ -57,24 +36,40 @@ static NSString *kPrefkeyPendingDeleteIDs = @"PendingDeleteIDs";
 	TRACE(@"deleteFCMTokenForSenderID: %@", senderID);
 	[[FIRMessaging messaging] deleteFCMTokenForSenderID:senderID completion:^(NSError *error) {
 		TRACE(@"deleteFCMTokenForSenderID: %@, error=%@", senderID, error);
-		if ([error.domain isEqualToString:NSURLErrorDomain]) {
-			[self.pendingDeleteIds addObject:senderID];
-			[self save];
+		if (error != nil && [error.domain isEqualToString:NSURLErrorDomain]) {
+			/*
+			 * Failed because of network problems. Add to pending list, so that
+			 * another attempt is made later.
+			 */
+			NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+			NSMutableArray *pending = [[defaults stringArrayForKey:kPrefkeyPendingDeleteIDs] mutableCopy];
+			if (pending == nil) {
+				pending = [NSMutableArray array];
+			}
+			[pending addObject:senderID];
+			[defaults setObject:pending forKey:kPrefkeyPendingDeleteIDs];
 		}
 	}];
 }
 
+/*
+ * Start a timer which regularly retries to delete outdated FCM tokens.
+ */
 - (void)resume {
 #ifdef DEBUG
-	uint64_t interval = 10 * NSEC_PER_SEC;
+	uint64_t interval = 10 * NSEC_PER_SEC; // 10 seconds
 #else
-	uint64_t interval = 60 * NSEC_PER_SEC;
+	uint64_t interval = 60 * NSEC_PER_SEC; // 1 minute
 #endif
 	self.timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_main_queue());
 	dispatch_source_set_timer(self.timer, DISPATCH_TIME_NOW, interval, interval);
 	dispatch_source_set_event_handler(self.timer, ^{
-		NSArray *pending = self.pendingDeleteIds;
-		self.pendingDeleteIds = [NSMutableArray array];
+		
+		NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+		NSArray *pending = [defaults stringArrayForKey:kPrefkeyPendingDeleteIDs];
+		[defaults removeObjectForKey:kPrefkeyPendingDeleteIDs];
+		TRACE(@"%ld pending entries", pending.count);
+
 		for (NSString *senderID in pending) {
 			[self deleteTokenFor:senderID];
 		}
