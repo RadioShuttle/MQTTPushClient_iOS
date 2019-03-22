390f98c56f145cd07d2adf659f509f769956a2ad
martin <>
2019-03-22 14:38:16 +0000

    Fixed example script: msg.raw is already an ArrayBuffer.

--- MQTTPushClient/Scripts/ExampleBinary.js
+++ MQTTPushClient/Scripts/ExampleBinary.js
@@ -1,6 +1,6 @@
 /* 12 bytes: uint16, uint16, float32, float32 (little endian) */
-if (msg.raw.length >= 12) {
-	var dv = new DataView(msg.raw.buffer);
+if (msg.raw.byteLength >= 12) {
+	var dv = new DataView(msg.raw);
 	if (dv.getUint16(0, true) == 33841) {
 		var t = dv.getFloat32(4, true);
 		var h = dv.getFloat32(8, true);
