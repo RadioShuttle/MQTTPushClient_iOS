fbfd885e2205bdf02d8ca0b15f31e9501599f27e
adalbert-hel <32635945+adalbert-hel@users.noreply.github.com>
2021-09-17 15:57:31 +0200

    Dashboard custom item: initWithJSON implementation

--- MQTTPushClient/Dashboard/Items/DashCustomItem.m
+++ MQTTPushClient/Dashboard/Items/DashCustomItem.m
@@ -5,7 +5,33 @@
  */
 
 #import "DashCustomItem.h"
+#import "NSDictionary+HelSafeAccessors.h"
 
 @implementation DashCustomItem
+
+- (instancetype)init
+{
+	self = [super init];
+	if (self) {
+		_parameter = [NSMutableArray new];
+	}
+	return self;
+}
+
+- (instancetype)initWithJSON:(NSDictionary *) dictObj {
+	self = [self init];
+
+	self.html = [dictObj helStringForKey:@"html"];
+
+	NSArray *parameterListJSON = [dictObj helArrayForKey:@"parameter"];
+	NSString *listItem;
+	for(int i = 0; i < [parameterListJSON count]; i++) {
+		listItem = parameterListJSON[i];
+		if (listItem) {
+			[self.parameter addObject:listItem];
+		}
+	}
+	return self;
+}
 
 @end
