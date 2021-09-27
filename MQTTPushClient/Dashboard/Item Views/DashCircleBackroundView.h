c687dd8e8655ca33b58c526ee53536ea460ffeb5
adalbert-hel <32635945+adalbert-hel@users.noreply.github.com>
2021-09-27 16:34:58 +0200

    init

---
+++ MQTTPushClient/Dashboard/Item Views/DashCircleBackroundView.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021 HELIOS Software GmbH
+ * 30827 Garbsen (Hannover) Germany
+ * Licensed under the Apache License, Version 2.0
+ */
+
+#import <UIKit/UIKit.h>
+
+/* transparent background and white circle (default) */
+@interface DashCircleBackroundView : UIView
+@property UIColor *fillColor;
+@end
